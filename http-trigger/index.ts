1bcdad6dd68d1b263ac8df0653dee091fe250f28
John Shew <johnshew@users.noreply.github.com>
2016-11-08 15:12:38 -0800

    Output something

--- http-trigger/index.ts
+++ http-trigger/index.ts
@@ -3,6 +3,7 @@
 import {HttpContext, IFunctionRequest, HttpStatusCodes} from 'azure-functions-typescript'
 
 export function index(context: HttpContext, req: IFunctionRequest) {
+    context.log('Running');
     if(req.method == "GET")
     context.res.status = HttpStatusCodes.OK;
     context.done(null, {});
